186c9ab7b7194bf8eaf6fdd3968e6bc28e0f5cc0
Sterling Clark <sterlingdclark13@gmail.com>
2021-08-31 13:55:29 -0400

    added instances and printed results for challenge_01

--- oop_sandbox/challenge_01.php
+++ oop_sandbox/challenge_01.php
@@ -15,13 +15,35 @@ class Bicycle {
     return floatval($this->weight_kg) * 2.2;
   }
 
-  function set_weight_lbs() {
-    
+  function set_weight_lbs($value) {
+    $this->weight_kg = floatval($value) / 2.2;
   }
 
 
 }
 
 
+$trek = new Bicycle;
+$trek->brand = 'Trek';
+$trek->model = 'Emonda';
+$trek->year = '2017';
+$trek->weight_kg = 1.0;
+
+$cd = new Bicycle;
+$cd->brand = 'Cannonda';
+$cd->model = 'Synap';
+$cd->year = '2019';
+$cd->weight_kg = 8.0;
+
+echo $trek->name() . "<br />";
+echo $cd->name() .  "<br />";
+
+echo $trek->weight_kg . "<br />";
+echo $trek->weight_lbs() . "<br />";
+// top is property, bottom is a method. 
+
+$trek->set_weight_lbs(2);
+echo $trek->weight_kg . "<br />";
+echo $trek->weight_lbs() . "<br />";
 
 ?>
