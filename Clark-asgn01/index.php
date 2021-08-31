2fedb7ff41772a4f377164b20efd16a883899a86
Sterling Clark <sterlingdclark13@gmail.com>
2021-08-31 14:40:11 -0400

    Finished asgn01

--- Clark-asgn01/index.php
+++ Clark-asgn01/index.php
@@ -7,13 +7,43 @@ class Bird {
   var $clutchSize;
 
   function birdSong() {
-    return 'drink-your-tea!';
+    return $this->birdSong . "<br />";
   }
 }
 
+
 $bird1 = new Bird;
+$bird1->commonName = 'Eastern Towee';
+$bird1->food = 'Seeds, Fruits, Insects, Spiders';
+$bird1->nestPlacement = 'Ground';
+$bird1->clutchSize = 2 - 6;
+$bird1->conservationLevel = 'Low';
+$bird1->birdSong = 'drink-your-tea!';
+
+$bird2 = new Bird;
+$bird2->commonName = 'Indigo Bunting';
+$bird2->food = 'Small seeds, Berries, Buds, and Insects';
+$bird2->nestPlacement = 'Fields and on the edges of woods, roadsides, and railroad
+rights-of-way.';
+$bird2->clutchSize = 3 - 4;
+$bird2->conservationLevel = 'Low';
+$bird2->birdSong = 'what! what!';
+
+echo "Bird 1: " . "<br />";
+echo $bird1->commonName . "<br />";
+echo $bird1->food . "<br />";
+echo $bird1->nestPlacement . "<br />";
+echo $bird1->clutchSize . "<br />";
+echo $bird1->conservationLevel . "<br />";
+echo $bird1->birdSong() . "<br />";
 
 
-
+echo "Bird 2: ". "<br />";
+echo $bird2->commonName . "<br />";
+echo $bird2->food . "<br />";
+echo $bird2->nestPlacement . "<br />";
+echo $bird2->clutchSize . "<br />";
+echo $bird2->conservationLevel . "<br />";
+echo $bird2->birdSong() . "<br />";
 
 ?>
