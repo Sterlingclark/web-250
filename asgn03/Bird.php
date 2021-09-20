d073d9502e7a07baf9e31f64ee09f7ecff31771b
Sterling Clark <sterlingdclark13@gmail.com>
2021-09-20 11:43:18 -0400

    Changed if else to use ternary operator and implemented static methods and properties

--- asgn03/Bird.php
+++ asgn03/Bird.php
@@ -1,31 +1,33 @@
 <?php
 
 class Bird {
-  var $habitat;
-  var $food;
-  var $nesting = "tree";
-  var $conservation;
-  var $song = "chirp";
-  var $flying = "yes";
+  public static $habitat;
+  public static $food;
+  public static $nesting = "tree";
+  public static $conservation;
+  public static $instatnce_count;
+  public static $egg_num = 0;
+  public static $song = "chirp";
+  private static $flying = "yes";
 
-  function can_fly() {
-    if ( $this->flying == "yes" ) {
-      $flying_string = "can fly";
-      } else {
-        $flying_string = "is stuck on the ground";
-      }
-      return  $flying_string ;
-    }
+
+  // I couldn't get the Null Coalescing Operator to work so I used 
+  // A Ternary Operator instead
+  public static function can_fly() {
+    $flying_string = (static::$flying) ? "can fly" : "is stuck on the ground"; 
+    return $flying_string;
+  }
 }
 
 class YellowBelliedFlyCatcher extends Bird {
-  var $name = "yellow-bellied flycatcher";
-  var $diet = "mostly insects.";
-  var $song = "flat chilk";
+  public static $name = "yellow-bellied flycatcher";
+  public static $diet = "mostly insects.";
+  public static $song = "flat chilk";
 }
 
 class Kiwi extends Bird {
-  var $name = "kiwi";
-  var $diet = "omnivorous";
-  var $flying = "no";
+  public static $name = "kiwi";
+  public static $diet = "omnivorous";
+  public static $flying = "no";
 }
+
