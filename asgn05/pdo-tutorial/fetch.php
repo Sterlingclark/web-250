328f828fe897f6b287e6d3f126ee9998701bf759
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-06 14:34:07 -0400

    Chagned echo statements in fetch.php and fetchAll.php

--- asgn05/pdo-tutorial/fetch.php
+++ asgn05/pdo-tutorial/fetch.php
@@ -4,7 +4,8 @@ include 'connect.php';
 $stmt = $db->query("SELECT * FROM names");
 
 while ($row = $stmt->fetch()) {
-  echo "<br>" . htmlentities($row['firstname']) .  " " . htmlentities($row['lastname']) . " " . htmlentities($row['postcode']) . "<br>";
+  // echo "<br>" . htmlentities($row['firstname']) .  " " . htmlentities($row['lastname']) . " " . htmlentities($row['postcode']) . "<br>";
+  echo "<pre>" . var_dump($row) . "</pre>";
 
 }
 
--- asgn05/pdo-tutorial/fetchAll.php
+++ asgn05/pdo-tutorial/fetchAll.php
@@ -6,7 +6,7 @@ $stmt = $db->query("SELECT * FROM names");
 $results = $stmt->fetchAll();
 
 while ($row = $stmt->fetch()) {
-  echo "<br>" . htmlentities($row['firstname']) .  " " . htmlentities($row['lastname']) . " " . htmlentities($row['postcode']) . "<br>";
+  echo "<pre>" . var_dump($row) . "</pre>";
 
 }
 
