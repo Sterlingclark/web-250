342096c01708a1ecbe4f0301367c615e05220cf5
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-07 10:02:39 -0400

    Added links to queries in index.php in  named-parameters folder

---
+++ asgn05/named-parameters/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+echo '<h1>Links for Queries</h1><br>';
+echo '<a href="query1.php">Query 1</a><br>';
+echo '<a href="query2.php">Query 2</a><br>';
+echo '<a href="query3.php">Query 3</a><br>';
+echo '<a href="insert.php">Insert</a><br>';
+echo '<a href="delete.php">Delete</a><br>';
+
+?>
