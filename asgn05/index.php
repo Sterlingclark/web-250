bacdd430b7bfccfeae7ad7faf77e891576e9c92f
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-07 10:05:48 -0400

    Added links to other index.php files

--- asgn05/index.php
+++ asgn05/index.php
@@ -2,11 +2,8 @@
 
 
 echo '<h1>Links for other assignments</h1><br>';
-echo '<a href="pdo-tutorial/fetch.php">Fetch</a><br>';
-echo '<a href="pdo-tutorial/fetchAll.php">Fetch All</a><br>';
-echo '<a href="pdo-tutorial/prepared-statement.php">Prepared Statements</a><br>';
-echo '<a href="pdo-tutorial/insert.php">Insert</a><br>';
-echo '<a href="pdo-tutorial/update.php">Update</a><br>';
-echo '<a href="pdo-tutorial/delete.php">Delete</a><br>';
+echo '<a href="pdo-tutorial/index.php">PDO Tutorial</a><br>';
+echo '<a href="named-parameters/index.php">Named Parameters Assignment</a><br>';
+
 
 ?>
---
+++ asgn05/pdo-tutorial/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+echo '<h1>Links for other assignments</h1><br>';
+echo '<a href="fetch.php">Fetch</a><br>';
+echo '<a href="fetchAll.php">Fetch All</a><br>';
+echo '<a href="prepared-statement.php">Prepared Statements</a><br>';
+echo '<a href="insert.php">Insert</a><br>';
+echo '<a href="update.php">Update</a><br>';
+echo '<a href="delete.php">Delete</a><br>';
+
+?>
