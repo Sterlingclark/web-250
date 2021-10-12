3856d140ce57454ef8490f3892b671211b9a8694
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-12 12:26:02 -0400

    Changed connection files for asgn05 and started asgn06

--- asgn05/named-parameters/connect.php
+++ asgn05/named-parameters/connect.php
@@ -1,11 +1,11 @@
 <?php
 
 try{
-  $db = new PDO('mysql:host=localhost;dbname=birdsQueries;charset=utf8', 'utmqempsv8gnr', 'secretpassword!');
+  $db = new PDO('mysql:localhost;dbname=dby3s7yj1gzdta;charset=utf8', 'utmqempsv8gnr', 'kj$Ltp@QHjN*S7C');
   // var_dump($db);
 } catch(Exception $e) {
-  // echo $e->getMessage();
-  echo "An error has occured";
+  echo $e->getMessage();
+  // echo "An error has occured";
 }
 
 
--- asgn05/pdo-tutorial/connect.php
+++ asgn05/pdo-tutorial/connect.php
@@ -1,11 +1,11 @@
 <?php
 
 try{
-  $db = new PDO('mysql:host=localhost;dbname=testdb', 'utmqempsv8gnr', 'secretpassword!');
+  $db = new PDO('mysql:localhost;dbname=dbd8p5tq91jlch', 'utmqempsv8gnr', 'kj$Ltp@QHjN*S7C');
   // var_dump($db);
 } catch(Exception $e) {
-  // echo $e->getMessage();
-  echo "An error has occured";
+  echo $e->getMessage();
+  // echo "An error has occured";
 }
 
 
