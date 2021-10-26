24d8e58218694c11371ccfdbc27102cf8f6e5aad
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-26 13:35:25 -0400

    Finished working on the Delete section of CRUD.

--- chain_gang/private/classes/bicycle.class.php
+++ chain_gang/private/classes/bicycle.class.php
@@ -4,8 +4,8 @@ class Bicycle {
 //----------- Start of active record code ---------------//
 
   static protected $database; 
-
   static protected $db_collumns =['id', 'brand', 'model', 'year', 'category', 'color', 'gender', 'price', 'weight_kg', 'condition_id', 'description'];
+  public $errors = [];
 
   static public function set_database($database) {
     self::$database = $database;
@@ -55,7 +55,22 @@ class Bicycle {
     return $object;
   }
 
+  protected function validate() {
+    $this->errors = [];
+
+    if(is_blank($this->brand)) {
+      $this->errors[] = "Brand cannot be blank";
+    }
+
+    if(is_blank($this->model)) {
+      $this->errors[] = "Model cannot be blank";
+    }
+    return $this->errors;
+  }
   protected function create() {
+    $this->validate();
+    if(!empty($this->errors)) { return false; }
+
     $attributes = $this->sanitized_attributes();
     $sql = "INSERT INTO bicycles (";
     $sql .= join(', ', array_keys($attributes));
@@ -70,6 +85,9 @@ class Bicycle {
   } 
 
   protected function update() {
+    $this->validate();
+    if(!empty($this->errors)) { return false; }
+
     $attributes = $this->sanitized_attributes();
     $attribute_pairs = [];
     foreach($attributes as $key => $value) {
@@ -117,6 +135,20 @@ class Bicycle {
       $sanitized[$key] = self::$database->escape_string($value);
     }
     return $sanitized;
+  }
+
+  public function delete() {
+    $sql = "DELETE FROM bicycles ";
+    $sql .= "WHERE id='" . self::$database->escape_string($this->id) . "' ";
+    $sql .= "LIMIT 1";
+    $results = self::$database->query($sql);
+    return $results;
+    // After deleting the instance of the object will still exist
+    // even though the database record does not 
+    // this can be useful as in : 
+    // echo $user->first_name . " was deleter.";
+    // but for example we can't call $user->update() after
+    // calling $user->delete().
   }
   //----------- End of active record code ---------------//
 
