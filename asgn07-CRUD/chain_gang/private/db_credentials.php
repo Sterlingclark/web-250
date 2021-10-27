00f0c2a878a3a751665fe8e16f4de43496b26a72
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-27 13:22:22 -0400

    Moved chain_gang project to asgn07-CRUD folder

---
+++ asgn07-CRUD/chain_gang/private/db_credentials.php
@@ -0,0 +1,16 @@
+<?php
+// Live server info:
+
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "webuser");
+// define("DB_PASS", "x6X3Ws2TPt4WtTJd");
+// define("DB_NAME", "chain_gang");
+
+// Local server info:
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "webuser");
+define("DB_PASS", "x6X3Ws2TPt4WtTJd");
+define("DB_NAME", "chain_gang");
+
+?>
