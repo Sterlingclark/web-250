5f5937d560c90d17fe70d1be49fbc2980cac0856
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-27 13:29:21 -0400

    Changeed server info before pushing

--- asgn07-CRUD/chain_gang/private/db_credentials.php
+++ asgn07-CRUD/chain_gang/private/db_credentials.php
@@ -1,16 +1,16 @@
 <?php
-// Live server info:
+// Local server info:
 
 // define("DB_SERVER", "localhost");
 // define("DB_USER", "webuser");
 // define("DB_PASS", "x6X3Ws2TPt4WtTJd");
 // define("DB_NAME", "chain_gang");
 
-// Local server info:
+// Live server info:
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
+define("DB_USER", "uav7ers3td1dz");
 define("DB_PASS", "x6X3Ws2TPt4WtTJd");
-define("DB_NAME", "chain_gang");
+define("DB_NAME", "db526aqivk1qma");
 
 ?>
