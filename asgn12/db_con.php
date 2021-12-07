16a50a65edb1b42ee1be004ff7d570ed6d4f8989
Sterling Clark <sterlingdclark13@gmail.com>
2021-12-07 16:35:21 -0500

    Changed db connection.

--- asgn12/db_con.php
+++ asgn12/db_con.php
@@ -1,10 +1,19 @@
 <?php
 
+// Use for Live server:
 $sname = "localhost";
-$uname = "root";
-$password = "";
-$db_name = "image_db";
+$uname = "uav7ers3td1dz";
+$password = "Web250153456!";
+$db_name = "dbnmvh6vgl7wtn";
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+
+// Use for Localhost
+// $sname = "localhost";
+// $uname = "root";
+// $password = "";
+// $db_name = "image_db";
+// $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if(!$conn) {
   echo "Connection Failed!";
