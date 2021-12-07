971df1dbc273a8195832cbad235ada175e67758e
Sterling Clark <sterlingdclark13@gmail.com>
2021-12-07 17:56:59 -0500

    changed db connection info for asgn11

--- asgn11/private/db-credentials.php
+++ asgn11/private/db-credentials.php
@@ -1,16 +1,16 @@
 <?php
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "Web250153456!");
-define("DB_NAME", "birdsdb");
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "webuser");
+// define("DB_PASS", "Web250153456!");
+// define("DB_NAME", "birdsdb");
 
 // Live server info:
 // This will be the info you have to use to login as well. 
 
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "uav7ers3td1dz");
-// define("DB_PASS", "Web250153456!");
-// define("DB_NAME", "dbgzaybni3mk2q");
+define("DB_SERVER", "localhost");
+define("DB_USER", "uav7ers3td1dz");
+define("DB_PASS", "Web250153456!");
+define("DB_NAME", "dbgzaybni3mk2q");
 
 ?>
